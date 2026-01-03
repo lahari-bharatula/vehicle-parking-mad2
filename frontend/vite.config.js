21b310d03c073b4a27f7bf90e9c3b98e90f78e2a
Lahari Bharatula <bbn.sailaharib.213056917@gmail.com>
2026-01-03 16:06:25 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     // vueDevTools(),
   ],
+  base:"/vehicle-parking-mad2/"
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
