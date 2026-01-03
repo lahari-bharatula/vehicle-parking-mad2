eddc24bc4335e0555953e230ce578a5410f8f1f6
23f2004503-lahari <bbn.sailaharib.213056917@gmail.com>
2026-01-03 17:10:19 +0530

    fixed hash history import

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import SignupView from '../views/SignupView.vue'
 import UserdashView from '../views/UserdashView.vue'
